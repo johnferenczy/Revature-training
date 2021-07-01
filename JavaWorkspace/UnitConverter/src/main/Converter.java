d72539bc1661da20dbbafccaa65bf1e066667a35
johnferenczy <86669736+johnferenczy@users.noreply.github.com>
2021-07-01 17:03:50 -0400

    Project A completed
    Unit converter is complete

--- JavaWorkspace/UnitConverter/src/main/Converter.java
+++ JavaWorkspace/UnitConverter/src/main/Converter.java
@@ -3,8 +3,8 @@ package main;
 import java.util.Scanner;
 
 public class Converter {
-
 	public static void main(String[] args) {
+		
 		Scanner input = new Scanner(System.in);
 		int menuSelection = 0;
 		while(menuSelection != 4) {
@@ -16,13 +16,23 @@ public class Converter {
 //			}
 			switch(menuSelection) {
 			case 1:
-//				cups_to_tsp
+				System.out.println("Enter the number of cups that you would like to convert to teaspoons:");
+				int cups = input.nextInt();
+				int tsp = cups*48;
+				System.out.println(cups+" cups is equivalent to "+tsp+" teaspoons.");
 				break;
 			case 2:
-//				mi_to_km
+				System.out.println("Enter the number of miles that you would like to convert to kilometers:");
+				double mi = input.nextDouble();
+				double km = mi*1.609;
+				System.out.println(mi+" miles is equivalent to "+km+" kilometers.");
 				break;
 			case 3:
 //				usGal_to_impGal
+				System.out.println("Enter the number of US gallons that you would like to convert to Imperial gallons:");
+				double usgal = input.nextDouble();
+				double impgal = usgal*0.8327;
+				System.out.println(usgal+" US gallons is equivalent to "+impgal+" Imperial gallons.");
 				break;
 			case 4:
 				break;
